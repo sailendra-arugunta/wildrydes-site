65273519ecc7670c20f53942ab3a96e6bbc016cd
Sailendra02 <sailendrareddy02@gmail.com>
2024-12-11 22:59:16 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_pyZbOQFTl', 
-        userPoolClientId: '5hfi17flvka8ma0ane37aqh85e', 
-        region: 'us-east-1' 2
+        userPoolId: 'us-east-1_rnFuu4s9t', 
+        userPoolClientId: '33m4s8aea3dsr7kfg4r6mgpjbl', 
+        region: 'us-east-1',
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
